211525521901b572543e0f8adeefc95fec384d97
zicong521 <823891202@qq.com>
2020-10-26 20:43:11 +0800

    更改了task里面的部分代码

--- Project/freertos/Source/task.c
+++ Project/freertos/Source/task.c
@@ -1,7 +1,12 @@
 #include "FreeRTOS.h"
 #include "task.h"
+#include "FreeRTOSConfig.h"
 
-
-#if (con)
-
-#endif
+#if (configSUPPORT_STATIC_ALLOCATION == 1)
+TaskHandle_t xTaskCreateStatic(	TaskFunction_t pxTaskCode,
+					            const char * const pcName,
+					            const uint32_t ulStackDepth,
+					            void * const pvParameters,
+					            StackType_t * const puxStackBuffer,
+					            TCB_t * const pxTaskBuffer );
+#endif /* configSUPPORT_STATIC_ALLOCATION */
